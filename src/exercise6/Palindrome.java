3078fbcbc221f41c8e9f4cbfb08f451b33f12177
stukkki <>
2021-11-18 08:10:29 +0000

    Some message

--- src/exercise6/Palindrome.java
+++ src/exercise6/Palindrome.java
@@ -5,5 +5,16 @@ import java.util.Scanner;
 public class Palindrome {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        int reverse = 0;
+        int a = n;
+        while (a > 0) {
+          int next = a % 10;
+          reverse = reverse * 10 + next;
+          a = a / 10;
+        }
+        boolean palindrome = n == reverse;
+        System.out.println("Palindrome: " + palindrome);
     }
 }
+
--- src/exercise6/PasswordCheck.java
+++ src/exercise6/PasswordCheck.java
@@ -5,5 +5,24 @@ import java.util.Scanner;
 public class PasswordCheck {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        boolean is_valid = true;
+        int digit_counter = 0;
+        int length = 0;
+        do {
+            is_valid = true;
+            String password = input.next();
+            length = password.length();
+            for (int i = 0; i < length; i++) {
+                if (!Character.isLetterOrDigit((password.charAt(i)))) {
+                    is_valid = false;
+                    break;
+                } else if (Character.isDigit((password.charAt(i)))) {
+                    digit_counter++;
+                }
+            }
+
+        } while ((!is_valid) || digit_counter < 2 || length < 8 );
+
+        System.out.println("Password valid!");
     }
 }
